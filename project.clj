7cdab1addd31c87c200d9ecdea94b2fafa811a02
ewen <ewen.grosjean@free.fr>
2018-07-27 00:05:41 +0200

    lein -> tools.deps

--- project.clj
+++
@@ -1,6 +0,0 @@
-(defproject replique-pedestal "0.0.1"
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.9.293"]
-                 [io.pedestal/pedestal.service "0.5.1"]
-                 [io.pedestal/pedestal.immutant "0.5.1"]]
-  :resource-paths ["resources"])
